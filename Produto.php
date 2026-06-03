cae8ff0648ceb4cada5b96b3ad99f47cc674735c
maria eloize <152327260+mariaeloize@users.noreply.github.com>
2026-06-03 19:42:20 -0300

    Add methods to manage product quantity in Produto class

--- Produto.php
+++ Produto.php
@@ -9,5 +9,30 @@
             $this->quantidade = $quantidade;
             $this->nome = $nome;
             $this->valorUnitario = $valorUnitario;
+
+           public function getQuantidade()
+        {
+            return $this->quantidade;
+        }
+
+        public function setQuantidade($quantidade)
+        {
+            $this->quantidade = $quantidade;
+        }
+
+        public function adicionar($quant)
+        {
+            $this->quantidade += $quant;
+            echo "Adicionados $quant produtos. Estoque: $this->quantidade";
+        }
+
+        public function retirar($quant)
+        {
+            if ($quant <= $this->quantidade) {
+                $this->quantidade -= $quant;
+                echo "Retirados $quant produtos. Estoque: $this->quantidade";
+            } else {
+                echo "ESTOQUE INSUFICIENTE!!!";
+            }
         }
     }
