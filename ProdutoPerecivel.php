8323bef814f41d95e8d6cf0284773744558e3c65
maria eloize <152327260+mariaeloize@users.noreply.github.com>
2026-06-03 19:45:04 -0300

    Implement ProdutoPerecivel class with storage

--- ProdutoPerecivel.php
+++ ProdutoPerecivel.php
@@ -1,3 +1,39 @@
 <?php
-    class ProdutoPerecivel extends Produto{
+
+    require "Produto.php";
+
+    class ProdutoPerecivel extends Produto
+    {
+        private $dataArmazenamento;
+
+        public function __construct($nome, $quantidade, $valorUnitario, $dataArmazenamento)
+        {
+            parent::__construct($nome, $quantidade, $valorUnitario);
+            $this->dataArmazenamento = $dataArmazenamento;
+        }
+
+        public function retirar($quant)
+        {
+            $armazenado = new DateTime($this->dataArmazenamento);
+            $hoje = new DateTime();
+
+            $meses = $armazenado->diff($hoje)->m +
+                    ($armazenado->diff($hoje)->y * 12);
+
+            if ($meses >= 2) {
+                $this->setQuantidade(0);
+                echo "PRODUTO VENCIDO!!! ESTOQUE ZERADO!!!";
+            } else {
+                parent::retirar($quant);
+            }
+        }
+
+        public function adicionar($quant)
+        {
+            if ($this->getQuantidade() == 0) {
+                parent::adicionar($quant);
+            } else {
+                echo "Não é possível adicionar ainda existe no ESTOQUE!!!";
+            }
+        }
     }
